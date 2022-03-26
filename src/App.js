593d6b12be04be468a1f40cb82936146a05defa2
Search <96184824+Saerch@users.noreply.github.com>
2022-03-27 00:24:15 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Details from './Components/Detail';
+import Search from './Components/Search';
+import axios from 'axios';
+import Results from './Components/Results';
 
 function App() {
+
+  const [state, setState] = useState({
+    s: "sherlock",
+    results: [],
+    selected: {},
+  });
+
+  
+  const apiurl ="http://www.omdbapi.com/?apikey=19e8cce2";
+  
+  const searchInput = (e) => {
+    let s = e.target.value;
+  
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+  };
+  
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+  
+        console.log(results);
+  
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+  };
+  
+  const openDetail = (id) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+  
+      setState((prevState) => {
+        return { ...prevState, selected: result };
+      });
+    });
+  };
+  
+  const closeDetail = () => {
+    setState((prevState) => {
+      return { ...prevState, selected: {} };
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>Movie List App</h1>
+    </header>
+    <main>
+      <Search searchInput={searchInput} search={search} />
+
+      <Results results={state.results} openDetail={openDetail} />
+
+      {typeof state.selected.Title != "undefined" ? (
+        <Details selected={state.selected} closeDetail={closeDetail} />
+      ) : (
+        false
+      )}
+    </main>
+  </div>
   );
 }
 
---
+++ src/Components/Search.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Search.css";
+function search({ searchInput, search }){
+
+return(
+<div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search for a Movie..."
+        className="search"
+        onChange={searchInput}
+        onKeyPress={search}
+      />
+    </div>
+
+)
+
+}
+
+export default search;
